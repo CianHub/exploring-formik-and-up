93a3b02787841f9e2b68684964a3e1b505244099
Cian O'Gralaigh <ihatecian@gmail.com>
2021-01-16 12:42:30 +0000

    remove boilerplate

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders with no error', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const app = screen.getByRole('application');
+  expect(app).toBeInTheDocument();
 });
